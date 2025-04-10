96b92b9be4e4a7f60fb922ea2da991b477c190a7
Samuel Marxer <ms6903@mci4me.at>
2025-04-10 23:01:41 +0200

    Aufgabe 62

--- my_classes.py
+++ my_classes.py
@@ -1,26 +1,60 @@
-from my_functions import estimate_max_hr
-class Subject():
-    def __init__(self, first_name, last, sex):
-      self.first_name = first_name
-      self.last_name = last
-      self.sex = sex
+from my_functions import set_max_hr
 
-    def estimate_max_hr(self, age, sex):
-      """A function that estimates the maximum heart rate of a subject"""
-      return estimate_max_hr(age, sex)
-
-class Supervisor():
+class Person:
     def __init__(self, first_name, last_name):
-      self.first_name = first_name
-      self.last_name = last_name
+        self.first_name = first_name
+        self.last_name = last_name
 
-class Experiment():
+class Subject(Person):
+
+    def calculate_age(self, date_of_birth):
+        from datetime import datetime
+        today = datetime.today()
+        birth_date = datetime.strptime(date_of_birth, "%Y-%m-%d")
+        age = today.year - birth_date.year
+       
+        if today < birth_date.replace(year=today.year):
+            age -= 1
+        return age
+    
+    def estimate_max_hr(self, age, sex):
+        return set_max_hr(age, sex)
+    
+    def __init__(self, first_name, last_name, sex, date_of_birth):
+        super().__init__(first_name, last_name)
+        self.sex = sex
+        self.__age = self.calculate_age(str(date_of_birth))
+        self.__max_hr = self.estimate_max_hr(self.__age, sex)
+
+
+    def get_age(self):
+        return self.__age
+    
+    def get_max_hr(self):
+        return self.__max_hr
+        
+    
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+    
+class Experiment:
     def __init__(self, name, date):
-      self.name = name
-      self.data = data
+        self.name = name
+        self.date = date
+        self.subject = None
+        self.supervisor = None
 
     def add_subject(self, subject):
       self.subject = subject
 
     def add_supervisor(self, supervisor):
-      self.supervisor=supervisor
+      self.supervisor = supervisor
+
+    def __str__(self):
+        return (
+            f"Experiment: {self.name}, Date: {self.date}, "
+            f"Subject: {self.subject.first_name} {self.subject.last_name}, "
+            f"Supervisor: {self.supervisor.first_name} {self.supervisor.last_name}"
+        )
